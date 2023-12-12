d597b0c80c7871307bbec09fe0ba6637ed49d7e5
Alric_wsl <alric.conte@gmail.com>
2023-12-12 18:49:57 +0100

    Refactor code to filter and merge datasets for 1900 projection

--- p1/Day02/ex03/main.py
+++ p1/Day02/ex03/main.py
@@ -11,19 +11,14 @@ load_csv_module = importlib.import_module("Day02.ex00.load_csv")
 def main():
 	try:
 		# Load data
-		life_data = load_csv_module.load("/home/alric/Code/42_datascience/p1/Day02/life_expectancy_years.csv")
+		life_expectancy_data = load_csv_module.load("/home/alric/Code/42_datascience/p1/Day02/life_expectancy_years.csv")
 		income_data = load_csv_module.load("/home/alric/Code/42_datascience/p1/Day02/income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
 		
-		# Assume the year data is present in the CSV file, adjust if necessary
-		year_data = income_data.columns[1:].astype(int)
-		print(year_data)
-		print()
-		print(life_data)
-		print()
-		print(income_data)
-		print()
+		# Filter the data for the year 1900
+		income_1900 = income_data[['country', '1900']]
+		life_expectancy_1900 = life_expectancy_data[['country', '1900']]
 		# Call the function to plot the projection for the year 1900
-		plot_projection_life(year_data, life_data, income_data.iloc[:, 1:], income_data.iloc[:, 1:])
+		plot_projection_life(income_1900, life_expectancy_1900)
 	
 	except Exception as e:
 		print(e)
--- p1/Day02/ex03/projection_life.py
+++ p1/Day02/ex03/projection_life.py
@@ -1,42 +1,49 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
+import re
 
 import numpy as np
 
-def convert_population_string(population_str):
-	"""
-	Convert population strings with 'M' or 'k' suffix to numeric values.
-	Replace errors with NaN.
-	"""
-	try:
-		if 'M' in population_str:
-			return float(population_str.replace('M', '')) * 1e6
-		elif 'k' in population_str:
-			return float(population_str.replace('k', '')) * 1e3
-		else:
-			return float(population_str)
-	except (ValueError, TypeError):
-		# If conversion fails, return NaN
-		return np.nan
+def convert_k_m_to_number(value):
+    if isinstance(value, str):  # Check if the value is a string
+        value = value.strip()  # Trim whitespace
+        if re.match(r'^\d+\.?\d*k$', value.lower()):  # Match digit(s) followed by 'k'
+            return float(value[:-1]) * 1e3
+        elif re.match(r'^\d+\.?\d*m$', value.lower()):  # Match digit(s) followed by 'M'
+            return float(value[:-1]) * 1e6
+        else:
+            return float(value)
+    return value
 
-def plot_projection_life(year_data, life_data, gdp_data, population_data, year=1900):
-	# Filter data for the year 1900
-	year_1900_life = life_data[year_data == year]
-	year_1900_gdp = gdp_data[year_data == year]
-	# Filter data for the year 1900 and remove NaN values
-	year_1900_population = population_data[year_data == year].apply(convert_population_string).dropna()
+def plot_projection_life(income_1900, life_expectancy_1900):
+    # Merge the datasets on the country column
+    print('Dataset before merging:')
+    print('income_1900:')
+    print(income_1900)
+    print('life_expectancy_1900:')
+    print(life_expectancy_1900)
+    merged_data = pd.merge(income_1900, life_expectancy_1900, on='country', )
+    print('Dataset after merging:')
+    print(merged_data)
+    # Filter data for the year 1900 and remove NaN values
+    res = merged_data.apply(convert_k_m_to_number).dropna()
+    print(pd.concat([
+        res[res['country'] == 'Mongolia'],
+        res[res['country'] == 'Micronesia, Fed. Sts.'],
+        res[res['country'] == 'Denmark']
+    ]))
 
-	# Create scatter plot
-	plt.figure(figsize=(12, 8))
-	sns.scatterplot(x=year_1900_gdp, y=year_1900_life, size=year_1900_population, sizes=(50, 500), label=f'Year {year}')
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.scatter(merged_data['1900_x'], merged_data['1900_y'])  # Assuming 1900_x is GNP, 1900_y is life expectancy
+    plt.xscale('log')
 
-	# Set plot labels and title
-	plt.xlabel('Gross National Product (GNP)')
-	plt.ylabel('Life Expectancy (years)')
-	plt.title(f'Projection of Life Expectancy in relation to GNP ({year} - All Countries)')
+     # Set custom ticks
+    tick_values = [300, 1000, 10000]  # You can add more tick values here
+    plt.xticks(tick_values, [str(value) for value in tick_values])
 
-	# Add legend
-	plt.legend()
-
-	# Display the plot
-	plt.show()
+    plt.title("Life Expectancy vs GNP in 1900")
+    plt.xlabel("GNP (PPP, Inflation Adjusted)")
+    plt.ylabel("Life Expectancy (years)")
+    plt.show()
